dea94107a306a0f42b95137dacd51227b9f52b09
Adrián Benavente <benaventeadrian@gmail.com>
2022-12-20 02:40:20 -0300

    feat: update in real time

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 import { intervalToDuration } from 'date-fns';
 
-const {years, months, days, hours, minutes} = intervalToDuration({
-  start: new Date(2022, 11, 18, 15, 0, 0),
-  end: new Date(),
-});
-
 const D = document;
 const yearsSlot = D.querySelector('.years');
 const monthsSlot = D.querySelector('.months');
@@ -12,8 +7,18 @@ const daysSlot = D.querySelector('.days');
 const hoursSlot = D.querySelector('.hours');
 const minutesSlot = D.querySelector('.minutes');
 
-yearsSlot.innerHTML = years > 0 ? `${years} años, ` : '';
-monthsSlot.innerHTML = months > 0 ? `${months} meses, ` : '';
-daysSlot.innerHTML = days > 0 ? (days > 1 ? `${days} días, ` : `${days} día, `) : '';
-hoursSlot.innerHTML = hours > 0 ? `${hours} horas y ` : '';
-minutesSlot.innerHTML = `${minutes} minutos`;
+const fillSlots = () => {
+  const {years, months, days, hours, minutes} = intervalToDuration({
+    start: new Date(2022, 11, 18, 15, 0, 0),
+    end: new Date(),
+  });
+  yearsSlot.innerHTML = years > 0 ? `${years} años, ` : '';
+  monthsSlot.innerHTML = months > 0 ? `${months} meses, ` : '';
+  daysSlot.innerHTML = days > 0 ? (days > 1 ? `${days} días, ` : `${days} día, `) : '';
+  hoursSlot.innerHTML = hours > 0 ? `${hours} horas y ` : '';
+  minutesSlot.innerHTML = `${minutes} minutos`;
+};
+
+window.addEventListener('load', fillSlots);
+
+setInterval(fillSlots, 1000);
